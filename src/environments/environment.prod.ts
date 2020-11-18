670f8f3189185055520ddc0f3815b6ca01e2f0e0
Karthi-SRV <karthivkm619@gmail.com>
2020-11-18 14:56:07 +0530

    Url changed for the production site

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  endPointApi: 'https://api.themoviedb.org/',
+  endPointApi: 'https://api.themoviedb.org',
   apiKey: 'e309d07146be1278db083daf1519cd84',
   versionNumber: '3'
 };
